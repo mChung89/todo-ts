992f6541d5e7a950bbebf7e0b7019c8e1bb95c27
Michael Chung <mikechung89@gmail.com>
2022-07-26 22:16:08 -0400

    adding todo adds to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import InputField from './components/InputField'
+import { Todo } from './model'
 import './App.css';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
-  console.log(todo)
+  const [todos,setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(todo) {
+      setTodos([...todos,{id: Date.now(), todo, isDone: false}])
+    }
+  }
+
   return (
     <div className="App">
       <span className='heading'>To-Do App</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <Todolist /> */}
+      {todos.map(each => <li>{each.todo}</li>)}
     </div>
   );
 }
